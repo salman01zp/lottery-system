d4554e73f04755f45c4639eecab150be3f86d0e0
salman01zp <pathansalman555@gmail.com>
2025-11-28 13:32:06 +0700

    add tests to initialize config

--- tests/lottery-system.ts
+++ tests/lottery-system.ts
@@ -4,13 +4,33 @@ import { LotterySystem } from "../target/types/lottery_system";
 
 describe("lottery-system", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider  = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const wallet = provider.wallet;
+
 
   const program = anchor.workspace.lotterySystem as Program<LotterySystem>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("should initialize config!", async () => {
+      const initConfigTx = await program.methods.initializeConfig(
+        new anchor.BN(0),
+        new anchor.BN(1864310488),
+        new anchor.BN(10000) // 10000 lamports per ticket
+      ).instruction();
+
+
+      const blockHashWithContext = await provider.connection.getLatestBlockhashAndContext();
+      const tx = new anchor.web3.Transaction({
+        feePayer : wallet.publicKey,
+        blockhash : blockHashWithContext.value.blockhash,
+        lastValidBlockHeight: blockHashWithContext.value.lastValidBlockHeight
+ 
+      }).add(initConfigTx)
+
+
+      const signature = await anchor.web3.sendAndConfirmTransaction(provider.connection, tx, [wallet.payer]);
+
+      console.log("Transaction signature  ", signature);
   });
-});
+}); 
